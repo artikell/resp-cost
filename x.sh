8b2962ee541dc5217d20e77e32a6dbb64990adc4
lizhiqiang.sf <lizhiqiang.sf@bytedance.com>
2025-08-15 17:47:15 +0800

    feat: modify redis version

--- x.sh
+++ x.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-python3 testing.py --target valkey@8.1 --password respcost --addr localhost:7379
-python3 testing.py --target redis@5.0 --password respcost --addr localhost:7380
-python3 testing.py --target redis@6.0 --password respcost --addr localhost:7381
-python3 testing.py --target redis@7.0 --password respcost --addr localhost:7382
+python3 testing.py --target valkey@9.0 --password respcost --addr localhost:7390
+python3 testing.py --target valkey@8.1 --password respcost --addr localhost:7381
+python3 testing.py --target redis@5.0 --password respcost --addr localhost:6350
+python3 testing.py --target redis@6.0 --password respcost --addr localhost:6360
+python3 testing.py --target redis@7.0 --password respcost --addr localhost:6370
+python3 testing.py --target redis@8.0 --password respcost --addr localhost:6380
